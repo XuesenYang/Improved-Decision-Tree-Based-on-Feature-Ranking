074fbd4327988ab84f679b3828c64ab2462cb2c3
XuesenYang <42894881+XuesenYang@users.noreply.github.com>
2019-05-09 21:08:29 +0800

    Update discreteOrContinue.m

--- discreteOrContinue.m
+++ discreteOrContinue.m
@@ -1,18 +1,18 @@
 function discrete_dim=discreteOrContinue(train_features,thres_disc)
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %判断某个维度的特征是不是连续取值
-    %train_features:训练集的特征
-    %thres_disc:离散特征阈值，>thres_disc认定为该特征取值范围连续
+    % Determine whether the characteristics of a dimension are continuous values
+    % train_features:      features of Training Sets
+    % thres_disc:          Discrete feature threshold, > thres_disc is recognized as continuous range of feature values
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
        
     fea=size(train_features,1);
     discrete_dim = zeros(1,fea);
-    for i = 1:fea  %遍历每个特征  
+    for i = 1:fea  % Traversing through each feature  
         Ub = unique(train_features(i,:)); 
         Nb = length(Ub);   
         if (Nb <= thres_disc)    
-            discrete_dim(i) = Nb; %得到训练样本中，这个特征的无重复的特征值的数目 存放在discrete_dim(i)中，i表示第i个特征  
+            discrete_dim(i) = Nb; % In the training sample, the number of non-repetitive eigenvalues of this feature is stored in discrete_dim(i), I denotes the first feature.  
         end    
     end
 end
